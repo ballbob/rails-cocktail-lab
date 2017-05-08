a476fe3b4989d11279f1b6608ab79637f73de430
Chris <cbrown605@googlemail.com>
2017-05-08 14:32:12 +0100

    Added initial value and routes.

---
+++ db/migrate/20170508132859_create_cocktails.rb
@@ -0,0 +1,10 @@
+class CreateCocktails < ActiveRecord::Migration
+  def change
+    create_table :cocktails do |t|
+      t.string :name
+      t.string :ingredient
+
+      t.timestamps null: false
+    end
+  end
+end
